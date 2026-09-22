f2d1295db5a56d7a4a93cbe578e2ca1ea6da4292
bikramroyutsa <110381880+bikramroyutsa@users.noreply.github.com>
2026-09-23 00:57:23 +0600

    Write partial bpe

--- parser.cpp
+++ parser.cpp
@@ -1,5 +1,7 @@
 #include <fstream>
 #include<iostream>
+#include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
 string to_lowercase(string s){
@@ -33,18 +35,51 @@ void printVector(vector<string>& vect){
     }
     cout << endl;
 }
+
+void bpe(vector<string>& allWords){
+
+    unordered_map<string, int> distinctWordCounts;
+    unordered_set<char> vocab;
+    vector<vector<string>> wordsBrokenDown;
+
+    for(string& s: allWords){
+        for(char& c: s){
+            vocab.insert(c);
+        }
+        auto it = distinctWordCounts.find(s);
+        if (it != distinctWordCounts.end()) {
+            distinctWordCounts[s] += 1;
+        }else{
+            vector<string> brokenWord;
+            for(char& c: s){
+                brokenWord.push_back(string(1, c));
+            }
+            brokenWord.push_back("$");
+            wordsBrokenDown.push_back(brokenWord);
+            distinctWordCounts[s] = 1;
+        }
+    }
+    for(vector<string>& s: wordsBrokenDown){
+        cout << "< ";
+        for(string c: s){
+            cout << c << ", ";
+        }
+        cout << ">" << endl;
+    }
+}
 int main(){
     ifstream file("input.txt");
     if(!file.is_open()){
         cout << "failed to open file" << endl;
     }
     string line;
-    vector<string> vocabs;
+    vector<string> allWords;
     while(getline(file, line)){
         vector<string> parsed = parseIntoWords(line);
-        vocabs.insert(vocabs.end(), parsed.begin(), parsed.end());
-        // printVector(parsed);
+        allWords.insert(allWords.end(), parsed.begin(), parsed.end());
+        // printVector(parsed); 
     }
-    printVector(vocabs);
+    // printVector(allWords);
+    bpe(allWords);
     return 0;
 }
