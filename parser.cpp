dc1cb152b355653bd0893eb3e27801aba3e5b547
bikramroyutsa <110381880+bikramroyutsa@users.noreply.github.com>
2026-09-23 16:47:16 +0600

    Finish bpe tokenization algorithm

--- parser.cpp
+++ parser.cpp
@@ -47,32 +47,8 @@ void printVector(vector<string>& vect){
     }
     cout << endl;
 }
-
-void bpe(vector<string>& allWords){
-    unordered_map<string, int> distinctWordCounts;
-    unordered_set<char> vocab;
-    vector<BrokenWord> wordsBrokenDown;
-    map<pair<string, string>, int> pairWiseCounts;
-
-    for(string& s: allWords){
-        for(char& c: s){
-            vocab.insert(c);
-        }
-        auto it = distinctWordCounts.find(s);
-        if (it != distinctWordCounts.end()) {
-            distinctWordCounts[s] += 1;
-        }else{
-            distinctWordCounts[s] = 1;
-        }
-    }
-    for(auto& it: distinctWordCounts){
-        vector<string> letters;
-        for(char c: it.first){
-            letters.push_back(string(1, c));
-        }
-        letters.push_back("$");
-        wordsBrokenDown.push_back(BrokenWord(it.first, letters, it.second));
-    }
+void getPairWiseCounts(vector<BrokenWord>& wordsBrokenDown, map<pair<string, string>, int>& pairWiseCounts){
+    pairWiseCounts = {};
     for(BrokenWord& bw: wordsBrokenDown){
         int len = bw.letters.size();
         for(int i = 0; i < len - 1; i++){
@@ -85,12 +61,87 @@ void bpe(vector<string>& allWords){
             }
         }
     }
-    vector<pair<string, string>> merges;
-    for(BrokenWord& bw: wordsBrokenDown){
-        
+}
+void merge(vector<pair<string, string>>& merges, vector<BrokenWord>& wordsBrokenDown
+    , map<pair<string, string>, int>& pairWiseCounts,
+    vector<string>& vocab
+){
+    pair<string, string> bestpair;
+    int maxCount = 0;
+    for(auto& it: pairWiseCounts){  
+        if(it.second > maxCount){
+            bestpair = it.first;
+            maxCount = it.second;
+        }
     }
-    for(auto& it: pairWiseCounts){
-        cout << "{" << it.first.first << "," << it.first.second << "}" << ":" << it.second << endl;
+    if(maxCount < 1){
+        return;
+    }
+    merges.push_back(bestpair);
+    vocab.push_back(bestpair.first + bestpair.second);
+    for(BrokenWord& it: wordsBrokenDown){
+        vector<string> newLetters;
+        int len = it.letters.size();
+
+        for(int i = 0; i < len; i++){
+            if(i < len - 1 && it.letters[i] == bestpair.first && it.letters[i + 1] == bestpair.second){
+                newLetters.push_back(it.letters[i] + it.letters[i + 1]);
+                i++; 
+            } else {
+                newLetters.push_back(it.letters[i]);
+            }
+        }
+        it.letters = newLetters;
+    }
+
+
+}
+void bpe(vector<string>& allWords){
+    unordered_map<string, int> distinctWordCounts;
+
+    vector<string> vocab;
+    vector<pair<string, string>> merges;
+
+    vector<BrokenWord> wordsBrokenDown;
+    map<pair<string, string>, int> pairWiseCounts;
+
+    unordered_set<string> uniqueChars;
+    for(string& s: allWords){
+        for(char& c: s){
+            uniqueChars.insert(string(1, c));
+        }
+        // auto it = distinctWordCounts.find(s);
+        // if (it != distinctWordCounts.end()) {
+        //     distinctWordCounts[s] += 1;
+        // }else{
+        //     distinctWordCounts[s] = 1;
+        // }
+        distinctWordCounts[s] += 1;
+    }
+    for(string s: uniqueChars){
+        vocab.push_back(s);
+    }
+    vocab.push_back("$");
+    for(auto& it: distinctWordCounts){
+        vector<string> letters;
+        for(char c: it.first){
+            letters.push_back(string(1, c));
+        }
+        letters.push_back("$");
+        wordsBrokenDown.push_back(BrokenWord(it.first, letters, it.second));
+    }
+    for(int i = 0; i < 100; i++){
+        getPairWiseCounts(wordsBrokenDown, pairWiseCounts);
+        merge(merges, wordsBrokenDown, pairWiseCounts, vocab);
+    }
+    // for(auto& it: wordsBrokenDown){
+    //     for(string& s: it.letters){
+    //         cout << s << ", ";
+    //     }
+    //     cout << endl;
+    // }
+    for(string s: vocab){
+        cout << s << " ";
     }
 }
 int main(){
